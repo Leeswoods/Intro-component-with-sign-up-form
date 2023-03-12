f476ecdafdaf57ad7f94ae6a85d8aa83b0bba018
Damien Ouvrard <ouvrard.damien33@gmail.com>
2023-03-12 14:12:48 +0100

    Commantaire Script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// On chercher à l'aide de querySelector la class Form
 const form = document.querySelector(".Form");
+
+// On va chercher les éléments du formulaires, les inputs
 const firstName = form.elements["First Name"];
 const lastName = form.elements["Last Name"];
 const email = form.elements["Email Address"];
 const password = form.elements["Password"];
 
+// Ecoute du bouton au moment du clique
 form.addEventListener("submit", (event) => {
   // Vérifier si les champs requis sont vides
   if (firstName.value === "") {
@@ -24,6 +28,7 @@ form.addEventListener("submit", (event) => {
   }
 
   // Vérifier si l'adresse électronique est formatée correctement
+  // Utilisation de la méthode Regex
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailRegex.test(email.value)) {
     showError(email, "Looks like this is not an email");
@@ -31,6 +36,7 @@ form.addEventListener("submit", (event) => {
   }
 });
 
+// Fonction qui montre les Erreurs
 function showError(input, message) {
   const errorDiv = input.parentElement.querySelector(".error-message");
   if (errorDiv) {
